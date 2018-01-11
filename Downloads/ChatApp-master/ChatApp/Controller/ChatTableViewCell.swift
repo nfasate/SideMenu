113b0fd27d29a502eae230bc4b0f6adcea45b84c
ptejas26 <ptejas.patelia@gmail.com>
2018-01-11 10:38:26 +0530

    commit

---
+++ Downloads/ChatApp-master/ChatApp/Controller/ChatTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ChatTableViewCell.swift
+//  ChatApp
+//
+//  Created by Nilesh's MAC on 1/11/18.
+//  Copyright © 2018 Nilesh's MAC. All rights reserved.
+//
+
+import UIKit
+
+class ChatTableViewCell: UITableViewCell {
+
+ @IBOutlet var lblsenderMessage : UILabel!
+@IBOutlet var lblreceiverMessage : UILabel!
+    @IBOutlet var lblDate : UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Downloads/ChatApp-master/ChatApp/Controller/UserChatTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  UserChatTableViewCell.swift
+//  ChatApp
+//
+//  Created by Nilesh's MAC on 1/10/18.
+//  Copyright © 2018 Nilesh's MAC. All rights reserved.
+//
+
+import UIKit
+
+class UserChatTableViewCell: UITableViewCell {
+
+    @IBOutlet var lblName : UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
